6106a17b290f1deaa00a1b0b04b8fb77e50d9b52
Menuka L.D.S <104838986+sanuthmenuka@users.noreply.github.com>
2023-10-24 08:46:55 +0530

    update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,35 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
-const {createTokens,validateToken} = require("../controllers/JWT");
+const { createTokens, validateToken } = require("../controllers/JWT");
 
 //controller functions
-const {loginUser,signupUser,getUserDetails,editProfile,addToLibrary,removeLibraryBook,createSubscription,success}=require('../controllers/userController')
+const {
+  loginUser,
+  signupUser,
+  getUserDetails,
+  editProfile,
+  addToLibrary,
+  removeLibraryBook,
+  createSubscription,
+  success,
+} = require("../controllers/userController");
 
-router.post('/login',loginUser)
+router.post("/login", loginUser);
 
-router.post('/signup',signupUser)
+router.post("/signup", signupUser);
 
-router.get('/getUserDetails',validateToken,getUserDetails)
+router.get("/getUserDetails", getUserDetails);
 
-router.post('/editProfile',validateToken,upload.any(),editProfile)
+router.post("/editProfile", validateToken, upload.any(), editProfile);
 
-router.get('/addToLibrary',validateToken,addToLibrary);
+router.get("/addToLibrary", validateToken, addToLibrary);
 
-router.delete('/removeLibraryBook/:id',validateToken,removeLibraryBook);
+router.delete("/removeLibraryBook/:id", validateToken, removeLibraryBook);
 
-router.post('/create-subscription',createSubscription);
+router.post("/create-subscription", createSubscription);
 
-router.get('/success',success);
+router.get("/success", success);
 
-module.exports=router
-
+module.exports = router;
